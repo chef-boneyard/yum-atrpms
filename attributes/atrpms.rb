4fc48fbd9c780103549232a851d1e907ac548912
Sean OMeara <someara@opscode.com>
2014-09-22 13:53:39 -0400

    updating for Amazon Linux and adding Fedora to testing harness

--- attributes/atrpms.rb
+++ attributes/atrpms.rb
@@ -1,13 +1,24 @@
 case node['platform_family']
 when 'rhel'
-  default['yum']['atrpms']['repositoryid'] = 'atrpms'
-  default['yum']['atrpms']['description'] = 'Red Hat Enterprise Linux $releasever - $basearch - ATrpms'
-  default['yum']['atrpms']['baseurl'] = 'http://dl.atrpms.net/el$releasever-$basearch/atrpms/stable'
-  default['yum']['atrpms']['gpgkey'] = 'http://ATrpms.net/RPM-GPG-KEY.atrpms'
-  default['yum']['atrpms']['failovermethod'] = 'priority'
-  default['yum']['atrpms']['gpgcheck'] = true
-  default['yum']['atrpms']['enabled'] = true
-  default['yum']['atrpms']['managed'] = true
+  if node['platform'] == 'amazon'
+    default['yum']['atrpms']['repositoryid'] = 'atrpms'
+    default['yum']['atrpms']['description'] = 'Red Hat Enterprise Linux $releasever - $basearch - ATrpms'
+    default['yum']['atrpms']['baseurl'] = 'http://dl.atrpms.net/el6-x86_64/atrpms/stable'
+    default['yum']['atrpms']['gpgkey'] = 'http://ATrpms.net/RPM-GPG-KEY.atrpms'
+    default['yum']['atrpms']['failovermethod'] = 'priority'
+    default['yum']['atrpms']['gpgcheck'] = true
+    default['yum']['atrpms']['enabled'] = true
+    default['yum']['atrpms']['managed'] = true
+  else
+    default['yum']['atrpms']['repositoryid'] = 'atrpms'
+    default['yum']['atrpms']['description'] = 'Red Hat Enterprise Linux $releasever - $basearch - ATrpms'
+    default['yum']['atrpms']['baseurl'] = 'http://dl.atrpms.net/el$releasever-$basearch/atrpms/stable'
+    default['yum']['atrpms']['gpgkey'] = 'http://ATrpms.net/RPM-GPG-KEY.atrpms'
+    default['yum']['atrpms']['failovermethod'] = 'priority'
+    default['yum']['atrpms']['gpgcheck'] = true
+    default['yum']['atrpms']['enabled'] = true
+    default['yum']['atrpms']['managed'] = true
+  end
 when 'fedora'
   default['yum']['atrpms']['repositoryid'] = 'atrpms'
   default['yum']['atrpms']['description'] = 'Fedora Core $releasever - $basearch - ATrpms'

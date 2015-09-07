c76b073c700a4b2b6066d1e075a853d559ec6a79
Tim Smith <tsmith@chef.io>
2015-09-07 13:24:00 -0700

    Update maintainer

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'yum-atrpms'
-maintainer 'Chef'
-maintainer_email 'Sean OMeara <someara@chef.io>'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io>'
 license 'Apache 2.0'
 description 'Installs/Configures yum-atrpms'
 version '0.1.0'

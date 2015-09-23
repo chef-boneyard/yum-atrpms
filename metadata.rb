c1b496b5594db1a22cf069828f395828d20d8db1
Tim Smith <tsmith84@gmail.com>
2015-09-22 22:59:50 -0700

    Update description, require yum 3.2, add supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,15 @@
 name 'yum-atrpms'
 maintainer 'Chef Software, Inc.'
-maintainer_email 'cookbooks@chef.io>'
+maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
-description 'Installs/Configures yum-atrpms'
+description 'Installs and configures the atrpms Yum repository'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
+source_url 'https://github.com/chef-cookbooks/yum-atrpms' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/yum-atrpms/issues' if respond_to?(:issues_url)
 
-depends 'yum', '~> 3.0'
+depends 'yum', '~> 3.2'
 
-supports 'redhat'
-supports 'centos'
-supports 'scientific'
-supports 'amazon'
-supports 'fedora'
+%w(amazon centos fedora oracle redhat scientific).each do |os|
+  supports os
+end

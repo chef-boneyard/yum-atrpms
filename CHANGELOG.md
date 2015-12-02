789587af5bca91a2778965414a60ca0503ef922c
Sean OMeara <sean@chef.io>
2015-12-01 21:57:15 -0500

    updating CHANGELOG and bumping metadata

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@ yum-atrpms Cookbook CHANGELOG
 ======================
 This file is used to list changes made in each version of the yum-atrpms cookbook.
 
+v0.1.3 (2015-12-01)
+-------------------
+- Fixing if/unless logic in recipes
+
 v0.1.2 (2015-10-28)
 -------------------
 - Fixing Chef 13 warnings by not passing nils
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs and configures the atrpms Yum repository'
-version '0.1.2'
+version '0.1.3'
 source_url 'https://github.com/chef-cookbooks/yum-atrpms' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/yum-atrpms/issues' if respond_to?(:issues_url)
 

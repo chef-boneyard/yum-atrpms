fa302e51e3cb092f035baa47638ef58fbb692d9a
Tim Smith <tsmith84@gmail.com>
2016-03-30 10:12:39 -0700

    Testing updates

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,12 @@ maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs and configures the atrpms Yum repository'
 version '0.1.3'
-source_url 'https://github.com/chef-cookbooks/yum-atrpms' if respond_to?(:source_url)
-issues_url 'https://github.com/chef-cookbooks/yum-atrpms/issues' if respond_to?(:issues_url)
 
 depends 'yum', '~> 3.2'
 
 %w(amazon centos fedora oracle redhat scientific).each do |os|
   supports os
 end
+
+source_url 'https://github.com/chef-cookbooks/yum-atrpms' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/yum-atrpms/issues' if respond_to?(:issues_url)

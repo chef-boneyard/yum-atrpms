bf376d9c46bf24343d5c212c6aaf97160d39f636
Tim Smith <tsmith84@gmail.com>
2016-04-18 22:42:47 -0700

    Resolve rubocop warnings

--- attributes/atrpms.rb
+++ attributes/atrpms.rb
@@ -4,28 +4,19 @@ when 'rhel'
     default['yum']['atrpms']['repositoryid'] = 'atrpms'
     default['yum']['atrpms']['description'] = 'Red Hat Enterprise Linux $releasever - $basearch - ATrpms'
     default['yum']['atrpms']['baseurl'] = 'http://dl.atrpms.net/el6-x86_64/atrpms/stable'
-    default['yum']['atrpms']['gpgkey'] = 'http://ATrpms.net/RPM-GPG-KEY.atrpms'
-    default['yum']['atrpms']['failovermethod'] = 'priority'
-    default['yum']['atrpms']['gpgcheck'] = true
-    default['yum']['atrpms']['enabled'] = true
-    default['yum']['atrpms']['managed'] = true
   else
     default['yum']['atrpms']['repositoryid'] = 'atrpms'
     default['yum']['atrpms']['description'] = 'Red Hat Enterprise Linux $releasever - $basearch - ATrpms'
     default['yum']['atrpms']['baseurl'] = 'http://dl.atrpms.net/el$releasever-$basearch/atrpms/stable'
-    default['yum']['atrpms']['gpgkey'] = 'http://ATrpms.net/RPM-GPG-KEY.atrpms'
-    default['yum']['atrpms']['failovermethod'] = 'priority'
-    default['yum']['atrpms']['gpgcheck'] = true
-    default['yum']['atrpms']['enabled'] = true
-    default['yum']['atrpms']['managed'] = true
   end
 when 'fedora'
   default['yum']['atrpms']['repositoryid'] = 'atrpms'
   default['yum']['atrpms']['description'] = 'Fedora Core $releasever - $basearch - ATrpms'
   default['yum']['atrpms']['baseurl'] = 'http://dl.atrpms.net/f$releasever-$basearch/atrpms/stable'
-  default['yum']['atrpms']['gpgkey'] = 'http://ATrpms.net/RPM-GPG-KEY.atrpms'
-  default['yum']['atrpms']['failovermethod'] = 'priority'
-  default['yum']['atrpms']['gpgcheck'] = true
-  default['yum']['atrpms']['enabled'] = true
-  default['yum']['atrpms']['managed'] = true
 end
+
+default['yum']['atrpms']['gpgkey'] = 'http://ATrpms.net/RPM-GPG-KEY.atrpms'
+default['yum']['atrpms']['failovermethod'] = 'priority'
+default['yum']['atrpms']['gpgcheck'] = true
+default['yum']['atrpms']['enabled'] = true
+default['yum']['atrpms']['managed'] = true

4fc4a04cba985c9be246fb4258da41715794fc41
Tim Smith <tsmith@chef.io>
2016-12-22 10:34:33 -0800

    Depend on the latest compat_resource cookbook

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'Apache 2.0'
 description 'Installs and configures the atrpms Yum repository'
 version '2.0.0'
 
-depends 'compat_resource', '>= 12.14.6'
+depends 'compat_resource', '>= 12.16.3'
 
 %w(amazon centos fedora oracle redhat scientific).each do |os|
   supports os
